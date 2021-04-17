3cbee388b3296945d5ca0e072e0564db5696f7fc
Jorge Matias <jor_am07@hotmail.com>
2021-04-16 20:32:04 -0500

    Version de CRUD que funciona utilizando strings

--- main.py
+++ main.py
@@ -1,26 +1,104 @@
-
+import sys
 clients = 'pablo, ricardo, '
 
+
 def create_client(client_name):
-    global clients
-    clients += client_name
-    _add_comma()
+    global clients 
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client alredy is in the client\'s list')
 
 
 def list_clients():
     global clients
     print(clients)
 
+
+def update_client(client_name):
+    global clients
+    if client_name in clients:
+        updated_client_name = input('What is the updated name? ')
+        clients = clients.replace(client_name + ', ' , updated_client_name + ', ')
+        print('Modified')
+        print(clients)
+    else:
+        print('Client is not in clients list')
+
+def delete_client(client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ', ', '')
+        
+    else:
+        print('Client is not in clients list')
+
+def search_client(client_name):
+    clients_list = clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+
 def _add_comma():
     global clients
     clients += ','
 
+def _print_welcome():
+    print('Welcome to platzi ventas')
+    print('*'*50)
+    print('What would you do today?')
+    print('[C]reate client')
+    print('[L]ist clients')
+    print('[D]elete client')
+    print('[U]pdate client')
+    print('[S]earch client')
 
+def _ask_client_name():
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name: ')
+        
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return client_name
 
 if __name__ == '__main__':
-    list_clients()
+    _print_welcome()
+    command = input('')
+    command = command.upper()
+    if command == 'C':
+        client_name = _ask_client_name()
+        create_client(client_name)
+        list_clients()
+    elif command == 'L':
+        list_clients()
 
-    create_client('Matias')
-
-    list_clients()
+    elif command == 'D':
+        client_name = _ask_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _ask_client_name()
+        update_client(client_name)
+        list_clients()
+    elif command == 'S':
+        client_name = _ask_client_name()
+        found = search_client(client_name)
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print(f'The client: {client_name} is not in the client\'s list')
+    else:
+        print('El comando es inválido')
 
